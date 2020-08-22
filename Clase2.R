b41280c2940d691414e5f65a9b27b7ed2bca54ed
pmoller7 <pmoller@uc.cl>
2020-08-22 10:33:00 -0400

    Agregue el rmd para explorar plantas extintas

--- Clase2.R
+++ Clase2.R
@@ -1,3 +1,8 @@
 library(tidyverse)
+library(dplyr)
 library(kableExtra)
 library(knitr)
+
+plants <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv")
+actions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv")
+threats <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv")
